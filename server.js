961fc8b250d593217c9806402ad19930c69588ae
Jackie Davison <jackiedavison@Jackies-MacBook-Air.local>
2019-05-06 14:14:18 -0400

    cleaned up

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
 require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 
-// app.listen(PORT, function() {
-//   console.log("Friend Finder app is listening on PORT: " + PORT);
-// });
+app.listen(PORT, function() {
+  console.log("Friend Finder app is listening on PORT: " + PORT);
+});
